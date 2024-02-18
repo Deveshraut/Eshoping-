f9739ece92bed5f8122c7db2419d6374f3108f07
Deveshraut <deveshraut96@gmail.com>
2024-02-18 10:27:17 +0530

    MyOrderController, OrderDetailsController, PaymentController are created with required endpoints. Also passwordEncoder bean is created

---
+++ backend/shopproject1/src/main/java/com/shop/controller/OrderDetailsController.java
@@ -0,0 +1,10 @@
+package backend.shopproject1.src.main.java.com.shop.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin(origins = "*")
+@RestController
+public class OrderDetailsController {
+
+
+}
